6f475281f9942f1147ed9d22b9038b61b4d5f928
Mauricio Hernan Cabrera <mauriciohernancabrera@gmail.com>
2020-08-27 20:57:51 -0300

    base project

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app dark>
+    <v-app-bar fixed app color="primary" dark>
+      <v-toolbar-title v-text="title" />
+    </v-app-bar>
+
+    <v-main>
+      <nuxt />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Resume Generator',
+    }
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <h2 class="h1">About me</h2>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="12" md="6" class="py-0">
+        <v-text-field label="Full name" placeholder="Mauricio Hernan Cabrera" />
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="12" md="6" class="py-0">
+        <v-text-field label="Job title" placeholder="Software Engineer" />
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="12" md="6" class="py-0">
+        <v-textarea
+          label="Biography"
+          placeholder="I'm a full-stack developer, specialising in web platform and security. If you're a forward-thinking organisation that's supportive of diversity and great development practices, I'd love to hear from you!"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {}
+</script>
