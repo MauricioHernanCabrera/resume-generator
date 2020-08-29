d38a2c249b82a8b4ecf448ae8fd6296a2d73a5ff
Mauricio Hernan Cabrera <mauriciohernancabrera@gmail.com>
2020-08-28 21:17:50 -0300

    step 1 updated and step 2 added

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <v-row justify="center">
-      <v-col cols="12" md="10" lg="8">
+      <v-col cols="12" md="10" xl="8">
         <v-stepper v-model="currentStep" alt-labels>
           <v-stepper-header>
             <template v-for="stepNumber in maxStep">
@@ -10,41 +10,68 @@
                 :complete="currentStep > stepNumber"
                 :step="stepNumber"
                 :editable="true"
-              >
-                {{ stepNames[stepNumber] || 'Default Step' }}
-              </v-stepper-step>
+              >{{ stepNames[stepNumber] || 'Default Step' }}</v-stepper-step>
 
-              <v-divider
-                v-if="stepNumber !== maxStep"
-                :key="stepNumber"
-              ></v-divider>
+              <v-divider v-if="stepNumber !== maxStep" :key="stepNumber"></v-divider>
             </template>
           </v-stepper-header>
 
           <v-stepper-items>
             <v-stepper-content step="1">
               <v-row>
-                <v-col cols="12" class="py-0">
-                  <v-text-field
-                    v-model="form.fullName"
-                    label="Full name"
-                    placeholder="Mauricio Hernan Cabrera"
-                  />
-                </v-col>
-
-                <v-col cols="12" class="py-0">
+                <v-col cols="12" sm="6" class="py-0">
                   <v-text-field
                     v-model="form.jobTitle"
                     label="Job title"
                     placeholder="Software Engineer"
+                    filled
+                  />
+                </v-col>
+              </v-row>
+
+              <v-row>
+                <v-col cols="12" sm="6" class="py-0">
+                  <v-text-field
+                    v-model="form.firstName"
+                    label="First name"
+                    placeholder="Mauricio Hernan"
+                    filled
+                  />
+                </v-col>
+
+                <v-col cols="12" sm="6" class="py-0">
+                  <v-text-field
+                    v-model="form.lastName"
+                    label="Last name"
+                    placeholder="Cabrera"
+                    filled
+                  />
+                </v-col>
+
+                <v-col cols="12" sm="6" class="py-0">
+                  <v-text-field
+                    v-model="form.email"
+                    label="Job title"
+                    placeholder="Software Engineer"
+                    filled
+                  />
+                </v-col>
+
+                <v-col cols="12" sm="6" class="py-0">
+                  <v-text-field
+                    v-model="form.phone"
+                    label="Phone"
+                    placeholder="5493794883889"
+                    filled
                   />
                 </v-col>
 
                 <v-col cols="12" class="py-0">
                   <v-textarea
-                    v-model="form.biography"
-                    label="Biography"
+                    v-model="form.professionalSummary"
+                    label="Professional Summary"
                     placeholder="I'm a full-stack developer, specialising in web platform and security. If you're a forward-thinking organisation that's supportive of diversity and great development practices, I'd love to hear from you!"
+                    filled
                   />
                 </v-col>
 
@@ -61,7 +88,142 @@
 
             <v-stepper-content step="2">
               <v-row>
-                <v-col cols="12" class="py-0"> Experience </v-col>
+                <v-col cols="12" class="py-0">
+                  <v-expansion-panels multiple>
+                    <v-expansion-panel v-for="job in form.jobs" :key="job.id">
+                      <v-expansion-panel-header>
+                        <v-row>
+                          <v-col cols="12" class="py-0 mb-2">
+                            <span class="d-block font-weight-bold">
+                              {{ job.title? job.title : '(Not specified)' }}
+                              {{ job.title && job.employer? 'at' : '' }}
+                              {{job.employer}}
+                            </span>
+                          </v-col>
+                          <v-col cols="12" class="py-0">
+                            <span
+                              class="d-inline-block font-weight-medium grey--text text--lighten-1"
+                            >
+                              {{ job.startDate }}
+                              {{ job.startDate && job.endDate? '-' : '' }}
+                              {{ job.endDate }}
+                            </span>
+                            <span
+                              class="d-inline-block font-weight-medium grey--text text--lighten-1"
+                            ></span>
+                          </v-col>
+                        </v-row>
+                      </v-expansion-panel-header>
+
+                      <v-expansion-panel-content>
+                        <v-row>
+                          <v-col cols="12" sm="6" class="py-0">
+                            <v-text-field
+                              v-model="job.title"
+                              label="Job title"
+                              placeholder="Core Developer"
+                              filled
+                            />
+                          </v-col>
+
+                          <v-col cols="12" sm="6" class="py-0">
+                            <v-text-field
+                              v-model="job.employer"
+                              label="Employer"
+                              placeholder="Bootstrap"
+                              filled
+                            />
+                          </v-col>
+
+                          <v-col cols="6" sm="3" class="py-0">
+                            <v-menu
+                              v-model="job.startDateOpen"
+                              transition="scale-transition"
+                              offset-y
+                            >
+                              <template v-slot:activator="{ on, attrs }">
+                                <v-text-field
+                                  v-model="job.startDate"
+                                  label="Start date"
+                                  readonly
+                                  filled
+                                  v-bind="attrs"
+                                  v-on="on"
+                                ></v-text-field>
+                              </template>
+
+                              <v-date-picker
+                                v-model="job.startDate"
+                                no-title
+                                scrollable
+                                type="month"
+                              />
+                            </v-menu>
+                          </v-col>
+
+                          <v-col cols="6" sm="3" class="py-0">
+                            <v-menu
+                              v-model="form.endDateOpen"
+                              transition="scale-transition"
+                              offset-y
+                            >
+                              <template v-slot:activator="{ on, attrs }">
+                                <v-text-field
+                                  clearable
+                                  v-model="job.endDate"
+                                  label="End date"
+                                  placeholder="-"
+                                  readonly
+                                  filled
+                                  v-bind="attrs"
+                                  v-on="on"
+                                ></v-text-field>
+                              </template>
+
+                              <v-date-picker
+                                v-model="job.endDate"
+                                no-title
+                                scrollable
+                                type="month"
+                              />
+                            </v-menu>
+                          </v-col>
+
+                          <v-col cols="12" sm="6" class="py-0">
+                            <v-text-field
+                              v-model="job.url"
+                              label="Employer url"
+                              placeholder="https://bootstrap.com"
+                              filled
+                            />
+                          </v-col>
+
+                          <v-col cols="12" class="py-0">
+                            <v-textarea
+                              v-model="job.description"
+                              label="Description"
+                              placeholder="Bootstrap is the most popular web framework in the world, emphasizing accessibility for design-oriented staff and reliability across browsers."
+                              filled
+                            />
+                          </v-col>
+
+                          <v-col cols="12" class="py-0 d-flex">
+                            <v-spacer></v-spacer>
+                            <v-btn color="primary" text @click="deleteJob(job.id)">Delete</v-btn>
+                            <v-spacer></v-spacer>
+                          </v-col>
+                        </v-row>
+                      </v-expansion-panel-content>
+                    </v-expansion-panel>
+                  </v-expansion-panels>
+                </v-col>
+
+                <v-col cols="12">
+                  <v-btn color="primary" text block class="d-flex justify-start" @click="addJob">
+                    <v-icon class="mr-2">mdi-plus</v-icon>Add employment
+                  </v-btn>
+                </v-col>
+
                 <v-col cols="12" class="py-0">
                   <step-actions
                     :currentStep="currentStep"
@@ -75,7 +237,7 @@
 
             <v-stepper-content step="3">
               <v-row>
-                <v-col cols="12" class="py-0"> Projects </v-col>
+                <v-col cols="12" class="py-0">Projects</v-col>
                 <v-col cols="12" class="py-0">
                   <step-actions
                     :currentStep="currentStep"
@@ -89,7 +251,7 @@
 
             <v-stepper-content step="4">
               <v-row>
-                <v-col cols="12" class="py-0"> Skills </v-col>
+                <v-col cols="12" class="py-0">Skills</v-col>
                 <v-col cols="12" class="py-0">
                   <step-actions
                     :currentStep="currentStep"
@@ -103,7 +265,7 @@
 
             <v-stepper-content step="5">
               <v-row>
-                <v-col cols="12" class="py-0"> Open Source </v-col>
+                <v-col cols="12" class="py-0">Open Source</v-col>
                 <v-col cols="12" class="py-0">
                   <step-actions
                     :currentStep="currentStep"
@@ -124,6 +286,19 @@
 
 <script>
 import StepActions from '@/components/StepActions'
+import { findIndex } from 'lodash'
+
+const DEFAULT_JOB = {
+  title: '',
+  url: '',
+  employer: '',
+  startDate: new Date().toISOString().substr(0, 7),
+  endDate: null,
+  startDateOpen: false,
+  endDateOpen: false,
+  city: '',
+  description: '',
+}
 
 export default {
   name: 'Home',
@@ -134,15 +309,25 @@ export default {
 
   data() {
     return {
-      currentStep: 1,
+      currentStep: 2,
       maxStep: 5,
       form: {
-        fullName: '',
+        firstName: '',
+        lastName: '',
         jobTitle: '',
-        biography: '',
+        phone: '',
+        email: '',
+        jobs: [
+          {
+            id: '1',
+            ...DEFAULT_JOB,
+          },
+        ],
       },
+      date: new Date().toISOString().substr(0, 10),
+      menu: false,
       stepNames: {
-        1: 'About me',
+        1: 'Personal Details',
         2: 'Experience',
         3: 'Projects',
         4: 'Skills',
@@ -175,6 +360,18 @@ export default {
     generateResume() {
       this.currentStep = 1
       console.log('generateResume')
+    },
+
+    addJob() {
+      this.form.jobs.push({
+        id: Date.now(),
+        ...DEFAULT_JOB,
+      })
+    },
+
+    deleteJob(id) {
+      const jobIndex = findIndex(this.form.jobs, ['id', id])
+      this.form.jobs.splice(jobIndex, 1)
     },
   },
 }
